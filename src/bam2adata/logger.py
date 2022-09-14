cb817f112b7cd2cd7a1441ec1fd76b9cabc4620e
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2022-09-14 16:36:23 +0800

    add logger and console

---
+++ src/bam2adata/logger.py
@@ -0,0 +1,13 @@
+import logging
+from rich.logging import RichHandler
+from rich import print
+from rich.console import Console
+
+console = Console()
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[RichHandler(rich_tracebacks=True,
+                                          console=Console(stderr=True))])  # should split error > stderr
+logger = logging.getLogger("bam2adata")
+pretty_print = print.__call__
